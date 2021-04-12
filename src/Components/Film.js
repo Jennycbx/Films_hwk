ba3c740c7550f16b590ccaf40aac5d986fe90d48
Jenny Bulford <jennycbx@hotmail.com>
2021-04-12 15:03:52 +0100

    Added url property to  Film & FilmList

--- src/Components/Film.js
+++ src/Components/Film.js
@@ -1,7 +1,10 @@
 const Film = ({title}) => {
 
     return (
-        <h3>{title}</h3>
+        <>
+            <h3>{title}</h3>
+            <href>{url}</href>
+        </>
     )
 }
 
--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -3,7 +3,7 @@ import Film from './Film';
 const FilmList = ({films}) => {
 
     const filmNodes = films.map((film) => {
-        return <Film title={film.title} key={film.id}></Film>
+        return <Film url={film.url} key={film.id}>{film.title}</Film>
     })
 
     return (
